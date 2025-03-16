0e1862a6ef2e94a3c819ec89fd97a17157755d14
Patrik Rönnlund <ronnlund.patrik@gmail.com>
2025-03-16 13:13:57 +0100

    Quartz sync: Mar 16, 2025, 1:13 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "notes from the dao",
-    pageTitleSuffix: "",
+    pageTitleSuffix: "| notes from the dao",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
